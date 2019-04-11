afe9013c6c5d3e97f554d8a134b827f18bb0406f
java-Lee-chan <jwlee1994@163.com>
2019-04-11 21:42:40 +0800

    引入了抽象基础模板

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -22,11 +22,11 @@ from typeidea.custom_site import custom_site
 
 
 urlpatterns = [
-    path('', post_list),
-    path('category/<category_id>', post_list),
-    path('tag/<tag_id>', post_list),
-    path('post/<post_id>.html', post_detail),
-    path('links', links),
-    path('super_admin/', admin.site.urls),
-    path('admin/', custom_site.urls),
+    path('', post_list, name='index'),
+    path('category/<category_id>', post_list, name='category-list'),
+    path('tag/<tag_id>', post_list, name='tag-list'),
+    path('post/<post_id>.html', post_detail, name='post-detail'),
+    path('links', links, name='links'),
+    path('super_admin/', admin.site.urls, name='super-admin'),
+    path('admin/', custom_site.urls, name='admin'),
 ]
