0231e571b55f77e57987fd262e44f5c75b31eb1e
java-Lee-chan <jwlee1994@163.com>
2019-04-13 21:41:55 +0800

    升级至class-based view

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,50 +1,107 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 
 from .models import Post, Tag, Category
 from config.models import SideBar
 
 
 # Create your views here.
-def post_list(request, category_id=None, tag_id=None):
-    # content= 'post_list category_id={category_id}, tag_id={tag_id}'.format(
-    #     category_id=category_id,
-    #     tag_id=tag_id,
-    # )
-    #
-    # return HttpResponse(content)
-    # return render(request, 'blog/list.html', context={'name': 'post_list'})
-    tag = None
-    category = None
+# def post_list(request, category_id=None, tag_id=None):
+#     # content= 'post_list category_id={category_id}, tag_id={tag_id}'.format(
+#     #     category_id=category_id,
+#     #     tag_id=tag_id,
+#     # )
+#     #
+#     # return HttpResponse(content)
+#     # return render(request, 'blog/list.html', context={'name': 'post_list'})
+#     tag = None
+#     category = None
+#
+#     if tag_id:
+#         post_list, tag = Post.get_by_tag(tag_id)
+#     elif category_id:
+#         post_list, category = Post.get_by_category(category_id)
+#     else:
+#         post_list = Post.latest_posts()
+#
+#     context = {
+#         'category': category,
+#         'tag': tag,
+#         'post_list': post_list,
+#         'sidebars': SideBar.get_all()
+#     }
+#     context.update(Category.get_nav())
+#     return render(request, 'blog/list.html', context=context)
+#
+#
+# def post_detail(request, post_id=None):
+#     # return HttpResponse('detail')
+#     # return render(request, 'blog/detail.html', context={'name': 'post_detail'})
+#     try:
+#         post = Post.objects.get(id=post_id)
+#     except Post.DoesNotExist:
+#         post = None
+#
+#     context = {
+#         'post': post,
+#         'sidebars': SideBar.get_all()
+#     }
+#     context.update(Category.get_nav())
+#     return render(request, 'blog/detail.html', context=context)
 
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.latest_posts()
 
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': SideBar.get_all()
-    }
-    context.update(Category.get_nav())
-    return render(request, 'blog/list.html', context=context)
+class CommonViewMixin:
+    def get_content_data(self, **kwargs):
+        context = super().get_content_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+        })
+        context.update(Category.get_nav())
+        return context
 
 
-def post_detail(request, post_id=None):
-    # return HttpResponse('detail')
-    # return render(request, 'blog/detail.html', context={'name': 'post_detail'})
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
 
-    context = {
-        'post': post,
-        'sidebars': SideBar.get_all()
-    }
-    context.update(Category.get_nav())
-    return render(request, 'blog/detail.html', context=context)
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
