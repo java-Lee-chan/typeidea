5e6172f61775f338c75c0365852f423b4c2a02a3
java-Lee-chan <jwlee1994@163.com>
2019-04-16 22:29:37 +0800

    暂时进行到评论模块的增加

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,9 +1,12 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 
 from .models import Post, Tag, Category
 from config.models import SideBar
+from comment.forms import CommentForm
+from comment.models import Comment
 
 
 # Create your views here.
@@ -52,8 +55,8 @@ from config.models import SideBar
 
 
 class CommonViewMixin:
-    def get_content_data(self, **kwargs):
-        context = super().get_content_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context.update({
             'sidebars': SideBar.get_all(),
         })
@@ -105,3 +108,34 @@ class PostDetailView(CommonViewMixin, DetailView):
     template_name = 'blog/detail.html'
     context_object_name = 'post'
     pk_url_kwarg = 'post_id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'comment_form': CommentForm,
+            'comment_list': Comment.get_by_target(self.request.path),
+        })
+        return context
+
+
+class SearchView(IndexView):
+    def get_context_data(self):
+        context = super().get_context_data()
+        context.update({
+            'keyword': self.request.GET.get('keyword', '')
+        })
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.GET.get('keyword')
+        if not keyword:
+            return queryset
+        return queryset.filter(Q(title__icontains=keyword) | Q(desc__icontains=keyword))
+
+
+class AuthorView(IndexView):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        author_id = self.kwargs.get('owner_id')
+        return queryset.filter(owner_id=author_id)
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 
 # from blog.views import post_list, post_detail
-from blog.views import (IndexView, CategoryView, TagView, PostDetailView,)
-from config.views import links
+from blog.views import (IndexView, CategoryView, TagView, PostDetailView, SearchView, AuthorView)
+from comment.views import CommentView
+from config.views import LinkView
 from .custom_site import custom_site
 
 
@@ -31,7 +32,10 @@ urlpatterns = [
     path('category/<category_id>', CategoryView.as_view(), name='category-list'),
     path('tag/<tag_id>', TagView.as_view(), name='tag-list'),
     path('post/<post_id>.html', PostDetailView.as_view(), name='post-detail'),
-    path('links', links, name='links'),
+    path('search/', SearchView.as_view(), name='search'),
+    path('author/<owner_id>', AuthorView.as_view(), name='author'),
+    path('links/', LinkView.as_view(), name='links'),
+    path('comment/', CommentView.as_view(), name='comment'),
     path('super_admin/', admin.site.urls, name='super-admin'),
     path('admin/', custom_site.urls, name='admin'),
 ]
